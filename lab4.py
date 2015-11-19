488aae33c9198a21c150b061015f73eedb7a9e58
sydneyjia <cyndileo@sina.com>
2015-11-19 13:06:51 +0800

    Jia Yuchen test lab 4 (passed)

--- lab4.py
+++ lab4.py
@@ -16,13 +16,21 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 # Rewrite this code to use global constants, local variables and functions
-# Output the text to a file instead of printing it
+# Output the text to a file instead of printing it,
 
+PROVINCIAL_SALES_TAX_RATE = 0.05
+FEDERER_SALES_TAX_RATE = 0.025
+TOTAL_RATE = PROVINCIAL_SALES_TAX_RATE + FEDERER_SALES_TAX_RATE
+TOTAL_MULTIPLIER = 1 + TOTAL_RATE
 
 def bill_of_sale(purchase):
+    file_result = "file.result"
+    with  open("result.txt", "w") as file_result:
+        file_result.write ("Amount of purchase: {0:.2f}\n".format(purchase))
+        file_result.write("Provincial tax: {0:.2f}\n".format(purchase *PROVINCIAL_SALES_TAX_RATE ))
+        file_result.write ("Federal tax: {0:.2f}\n".format(purchase * FEDERER_SALES_TAX_RATE))
+        file_result.write ("Total tax: {0:.2f}\n".format(purchase * TOTAL_RATE))
+        file_result.write ("Total sale: {0:.2f}\n".format(purchase * TOTAL_MULTIPLIER))
 
-    print ("Amount of purchase: {0:.2f}".format(purchase))
-    print ("Provincial tax: {0:.2f}".format(purchase * .05))
-    print ("Federal tax: {0:.2f}".format(purchase * .025))
-    print ("Total tax: {0:.2f}".format(purchase * .075))
-    print ("Total sale: {0:.2f}".format(purchase * 1.075))
+
+bill_of_sale(100)
